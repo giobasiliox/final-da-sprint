ab39b20ca1bb4f63c5ef6d2f713b4cb0253ab074
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-06-03 14:06:06 -0300

    login na to-do list

--- assets/js/to-do.js
+++ assets/js/to-do.js
@@ -1,115 +1,165 @@
-//variável para prevenir que a primeira adição de informação não funcione 
+// alert("Olá, valide a sua entrada")
+// let user = prompt("Digite o nome de usuario")
+
+
+// if(user != "Fábio Vilas Boas"){
+//     alert("Você não faz parete da nossa equipe, a to-do list não estrá habilitada para você")
+//     console.log("caiu aqui")
+   
+// }else{
+//     let senha = prompt("Olá querido, digite a sua senha:")
+//     console.log("pediu senha")
+// if(senha =="PsiOmega123"){
+//     alert("Bem vindo")
+var usuarios = ['Fábio Vilas Boas', 'Gabriel Caurin', 'Giovana Basílio', 'Nicoly Val', 'Rafael Cumpri', 'Victor Hugo'];
+var senha = 'Vishkk007';
+var tentativas = 0;
+
+
+while (true){
+function promptLogin() {
+  var user = prompt('Digite o nome de usuário:');
+  var tentativasenha = prompt('Digite a senha:');
+  
+  if (usuarios.includes(user) &&tentativasenha === senha) {
+    alert("Bem Vindo")
+  } else {
+    tentativas++;
+    if (tentativas > 3) {
+      alert('Acesso bloqueado. Tente novamente mais tarde.');
+      false;
+    } else {
+      promptLogin();
+    }
+  }
+}
+
+promptLogin();
+
+// Restante do código da to-do list
+// ...
 var edit = -1
-
-//function para adição de noticia 
-function adicionarnot() {
-    //variáveis pegar valor escrito
-    var titulo = document.getElementById('titulo').value
-    var data = document.getElementById('data').value
-    var descricao = document.getElementById('descrição').value
-    var autor = document.getElementById('autor').value
-    //variável separar a data
-    var inteiro = data.split('-')
-
-    //verificar se as caixas de textos estão vazias
-    if (titulo == '' || data == '' || descricao == '' || autor == '') {
-        alert('insira valor valido')
-    } 
-    //verifica se o botão de editar foi apertado (caso for falso)
-    else if (edit < 0) {
-        //puxando os intens escritos para uma array
-        dias.push(inteiro[2])
-        meses.push(inteiro[1])
-        anos.push(inteiro[0])
-        titulos.push(titulo)
-        datas.push(data)
-        descricoes.push(descricao)
-        autores.push(autor)
-
-     //esvaziando variaveis
-        titulo = ''
-        data = ''
-        descricao = ''
-        autor = ''
-    
-        //esvaziando as caixas de texto
-        document.getElementById('titulo').value = ''
-        document.getElementById('data').value = ''
-        document.getElementById('descrição').value = ''
-        document.getElementById('autor').value = ''
-
-        //array dos textos adicionados
-        text[i] = `<section class="noticiToDo" id="noticia${i}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[i]}</h1><h5 class="titlesno">Data: ${dias[i]}/${meses[i]}/${anos[i]}</h5><p class="titlesno">Descrição: ${descricoes[i]}</p><h4 class="titlesno">Autor: ${autores[i]}</h4><button class="butdel" onclick="deletar(${i})">Deletar 🗑️</button><button class="butedit" onclick="editar(${i})">Editar ✏️</button></div></section>`
-
-        //adicionar outros textos
-        document.getElementById(`todos`).innerHTML = `${text.join(' ')}`
-        i++
-    }
-
-//verifica se o botão de editar foi apertado (caso verdadeiro)
-    if (edit > -1 ) {
-        //puxando os intens escritos para uma array
-        dias[edit] = inteiro[2]
-        meses[edit] = inteiro[1]
-        anos[edit] = inteiro[0]
-        titulos[edit] = titulo
-        datas[edit] = data
-        descricoes[edit] = descricao
-        autores[edit] = autor
-
-        //esvaziando as caixas de texto
-        document.getElementById('titulo').value = ''
-        document.getElementById('data').value = ''
-        document.getElementById('descrição').value = ''
-        document.getElementById('autor').value = ''
-
-         //array dos textos adicionados
-        text[edit] = `<section class="noticiToDo" id="noticia${edit}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[edit]} - editado</h1><h5 class="titlesno">Data: ${dias[edit]}/${meses[edit]}/${anos[edit]}</h5><p class="titlesno">Descrição: ${descricoes[edit]}</p><h4 class="titlesno">Autor: ${autores[edit]}</h4><button class="butdel" onclick="deletar(${edit})">Deletar 🗑️</button><button class="butedit" onclick="editar(${edit})">Editar ✏️</button></div></section>`
-       
-       //adicionar outros textos
-        document.getElementById(`todos`).innerHTML = `${text.join(' ')}`
-
-        //variável para completar 
-        edit = -1
-    }
+    //function para adição de noticia 
+    function adicionarnot() {
+      //variáveis pegar valor escrito
+      var titulo = document.getElementById('titulo').value
+      var data = document.getElementById('data').value
+      var descricao = document.getElementById('descrição').value
+      var autor = document.getElementById('autor').value
+      //variável separar a data
+      var inteiro = data.split('-')
+  
+      //verificar se as caixas de textos estão vazias
+      if (titulo == '' || data == '' || descricao == '' || autor == '') {
+          alert('insira valor valido')
+      } 
+      //verifica se o botão de editar foi apertado (caso for falso)
+      else if (edit < 0) {
+          //puxando os intens escritos para uma array
+          dias.push(inteiro[2])
+          meses.push(inteiro[1])
+          anos.push(inteiro[0])
+          titulos.push(titulo)
+          datas.push(data)
+          descricoes.push(descricao)
+          autores.push(autor)
+  
+       //esvaziando variaveis
+          titulo = ''
+          data = ''
+          descricao = ''
+          autor = ''
+      
+          //esvaziando as caixas de texto
+          document.getElementById('titulo').value = ''
+          document.getElementById('data').value = ''
+          document.getElementById('descrição').value = ''
+          document.getElementById('autor').value = ''
+  
+          //array dos textos adicionados
+          text[i] = `<section class="noticiToDo" id="noticia${i}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[i]}</h1><h5 class="titlesno">Data: ${dias[i]}/${meses[i]}/${anos[i]}</h5><p class="titlesno">Descrição: ${descricoes[i]}</p><h4 class="titlesno">Autor: ${autores[i]}</h4><button class="butdel" onclick="deletar(${i})">Deletar 🗑️</button><button class="butedit" onclick="editar(${i})">Editar ✏️</button></div></section>`
+  
+          //adicionar outros textos
+          document.getElementById(`todos`).innerHTML = `${text.join(' ')}`
+          i++
+      }
+  
+  //verifica se o botão de editar foi apertado (caso verdadeiro)
+      if (edit > -1 ) {
+          //puxando os intens escritos para uma array
+          dias[edit] = inteiro[2]
+          meses[edit] = inteiro[1]
+          anos[edit] = inteiro[0]
+          titulos[edit] = titulo
+          datas[edit] = data
+          descricoes[edit] = descricao
+          autores[edit] = autor
+  
+          //esvaziando as caixas de texto
+          document.getElementById('titulo').value = ''
+          document.getElementById('data').value = ''
+          document.getElementById('descrição').value = ''
+          document.getElementById('autor').value = ''
+  
+           //array dos textos adicionados
+          text[edit] = `<section class="noticiToDo" id="noticia${edit}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[edit]} - editado</h1><h5 class="titlesno">Data: ${dias[edit]}/${meses[edit]}/${anos[edit]}</h5><p class="titlesno">Descrição: ${descricoes[edit]}</p><h4 class="titlesno">Autor: ${autores[edit]}</h4><button class="butdel" onclick="deletar(${edit})">Deletar 🗑️</button><button class="butedit" onclick="editar(${edit})">Editar ✏️</button></div></section>`
+         
+         //adicionar outros textos
+          document.getElementById(`todos`).innerHTML = `${text.join(' ')}`
+  
+          //variável para completar 
+          edit = -1
+      }
+  }
+  
+  //declaração de variáveis
+  var titulos = []
+  var datas = []
+  var descricoes = []
+  var autores = []
+  var i = 0
+  var text = []
+  var dias = []
+  var meses = []
+  var anos = []
+  
+  //function para deletar
+  function deletar(x) {
+  
+      //esvazia a posição que deseja deletar
+      dias[x] = ''
+      meses[x] = ''
+      anos[x] = ''
+      titulos[x] = ''
+      datas[x] = ''
+      descricoes[x] = ''
+      autores[x] = ''
+      text[x] = ''
+  
+      //fazer desaparecer o item desejado
+      document.getElementById(`noticia${x}`).style.display = 'none'
+  }
+  
+  //fuction para editar o item desejado 
+  function editar(y) {
+      document.getElementById('titulo').value = titulos[y]
+      document.getElementById('data').value = datas[y]
+  
+      document.getElementById('descrição').value = descricoes[y]
+      document.getElementById('autor').value = autores[y]
+      edit = y
+  
+      text[y] = `<section class="noticiToDo" id="noticia${y}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[y]}</h1><h5 class="titlesno">Data: ${datas[y]}</h5><p class="titlesno">Descrição: ${descricoes[y]}</p><h4 class="titlesno">Autor: ${autores[y]}</h4><button class="butdel" onclick="deletar(${y})">Deletar 🗑️</button><button class="butedit" onclick="editar(${y})">Editar ✏️</button></div></section>`
+  }
 }
 
-//declaração de variáveis
-var titulos = []
-var datas = []
-var descricoes = []
-var autores = []
-var i = 0
-var text = []
-var dias = []
-var meses = []
-var anos = []
+// }else{
+//     alert("Saia daqui infeliz")
+// }
 
-//function para deletar
-function deletar(x) {
+// }
 
-    //esvazia a posição que deseja deletar
-    dias[x] = ''
-    meses[x] = ''
-    anos[x] = ''
-    titulos[x] = ''
-    datas[x] = ''
-    descricoes[x] = ''
-    autores[x] = ''
-    text[x] = ''
 
-    //fazer desaparecer o item desejado
-    document.getElementById(`noticia${x}`).style.display = 'none'
-}
 
-//fuction para editar o item desejado 
-function editar(y) {
-    document.getElementById('titulo').value = titulos[y]
-    document.getElementById('data').value = datas[y]
+//variável para prevenir que a primeira adição de informação não funcione 
 
-    document.getElementById('descrição').value = descricoes[y]
-    document.getElementById('autor').value = autores[y]
-    edit = y
-
-    text[y] = `<section class="noticiToDo" id="noticia${y}"><div class="separator"><h1 class="titlesno">Titulo: ${titulos[y]}</h1><h5 class="titlesno">Data: ${datas[y]}</h5><p class="titlesno">Descrição: ${descricoes[y]}</p><h4 class="titlesno">Autor: ${autores[y]}</h4><button class="butdel" onclick="deletar(${y})">Deletar 🗑️</button><button class="butedit" onclick="editar(${y})">Editar ✏️</button></div></section>`
-}
