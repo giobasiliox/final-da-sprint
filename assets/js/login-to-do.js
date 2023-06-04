41181585ce7eff8ac309f00bcf73f6ecac57ef6f
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-06-04 18:32:50 -0300

    javascript do login

---
+++ assets/js/login-to-do.js
@@ -0,0 +1,12 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Verifica se o nome de usuário e a senha estão corretos
+    if ((username == "Fábio" || username == "Giovana" || username == "Gabriel" || username == "Nicoly" || username == "Rafael" || username === "Victor") && password == "PsiOmega123") {
+        // Redireciona para a página da to-do list
+        window.location.href = "../pages/to-do-list.html";
+    } else {
+        document.getElementById("error-message").style.display = "block";
+    }
+}
