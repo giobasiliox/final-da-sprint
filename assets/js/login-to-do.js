1920f2a2655b1b786cb254fba8110c25138d310b
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-06-06 18:44:33 -0300

    ajustes no teste do menu lateral

--- assets/js/login-to-do.js
+++ assets/js/login-to-do.js
@@ -14,19 +14,20 @@ function visualizarsenha() {
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-<<<<<<< HEAD
+
 
     if ((username == "Professor" ||username == "Fábio_VilasBoas" || username == "Giovana_Basilio" || username == "Gabriel_Caurin" || username == "Nicoly_Val" || username == "Rafael_Cumpri" || username == "Victor_Hugo") && (password == "123"|| password == "prof123")) {
       
         window.location.href = "./to-do list.html";
     } else {
-=======
+
     if ((username == "Professor" || username == "Fábio_VilasBoas" || username == "Giovana_Basilio" || username == "Gabriel_Caurin" || username == "Nicoly_Val" || username == "Rafael_Cumpri" || username == "Victor_Hugo") && (password == "123" || password == "prof123")) {
         window.location.replace("./../../pages/to-do-list.html");
       } else {
->>>>>>> ddbde230ac1f2d7c280d83fa9833ae8207348a8b
+
         document.getElementById("mensagem").style.display = "block";
       }
       
       
+}
 }
