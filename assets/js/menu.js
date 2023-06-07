5192e0911c55d90f7e6d5c1f4d9ecb3ff1073cd5
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-06-06 22:16:22 -0300

    novo menu lateral

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,19 +1,22 @@
-'use strict'
+'use strict';
 
 const menuSize = '250px';
 
-let open = true;
+let open = false; // Alterado para 'false' para manter o menu fechado por padrão
+
+document.addEventListener('DOMContentLoaded', () => { // Adicionado um event listener para o evento 'DOMContentLoaded'
+    toggleMenu();
+});
 
 document.querySelector('#btnMenu').addEventListener('click', e => {
     open = !open;
     toggleMenu();
-})
+});
 
 document.querySelector('#btnClose').addEventListener('click', e => {
     open = false;
-
     toggleMenu();
-})
+});
 
 function toggleMenu() {
     if (open) {
